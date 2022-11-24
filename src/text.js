4952da02dcd9b537ce74bf37147d9b036ce751c9
uadam12 <uadam12@gmail.com>
2022-11-24 16:23:42 +0100

    update image url

--- src/text.js
+++ src/text.js
@@ -2,7 +2,7 @@ import reactStringReplace from "react-string-replace";
 
 export const text = str =>
   reactStringReplace(str, /(q[0-9]{1,3}[a-e]?\.png)/gi, (match, i) => {
-    const image = require("./images/"+match);
-    return <img className="img-fluid" src={image} alt={match} />
+    const image = process.env.PUBLIC_URL + "/images/" + match;
+    return <img className="img-fluid" src={image} alt={image} />
   }
   );
